b53019d47e0e017bbbad4890cd9701978e585172
Fenar Khan <fenarkhan7@gmail.com>
2023-10-04 00:50:48 +0530

    home slider fixed abouts us image added supaa

--- js/slider2.js
+++ js/slider2.js
@@ -1,50 +1,51 @@
 const prevButton = document.querySelector('.btn-prev');
 const nextButton = document.querySelector('.btn-next');
-const sliderContainer = document.querySelector('.slider-container');
 const slides = document.querySelectorAll('.slide');
 
 let currentIndex = 0;
+let transitioning = false;
 
 function showSlide(index) {
-  // Calculate the translation value for the slider container
-  const translateValue = -index * 100;
+  if (transitioning || index === currentIndex) return;
+  transitioning = true;
 
-  // Apply the animation to the slider container
-  sliderContainer.style.transform = `translateX(${translateValue}%)`;
-
-  // Remove the 'active' class from all slides
-  slides.forEach((slide) => {
-    slide.classList.remove('active');
-  });
-
-  // Add the 'active' class to the current slide
+  slides[currentIndex].classList.remove('active');
   slides[index].classList.add('active');
+
+  setTimeout(() => {
+    transitioning = false;
+    currentIndex = index;
+  }, 500); // Match the duration of the CSS transition
 }
 
 prevButton.addEventListener('click', () => {
-  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-  showSlide(currentIndex);
+  const newIndex = (currentIndex - 1 + slides.length) % slides.length;
+  showSlide(newIndex);
 });
 
 nextButton.addEventListener('click', () => {
-  currentIndex = (currentIndex + 1) % slides.length;
-  showSlide(currentIndex);
+  const newIndex = (currentIndex + 1) % slides.length;
+  showSlide(newIndex);
 });
 
 function autoSlide() {
-  currentIndex = (currentIndex + 1) % slides.length;
-  showSlide(currentIndex);
+  const newIndex = (currentIndex + 1) % slides.length;
+  showSlide(newIndex);
 }
 
 // Automatically advance the slider every 3 seconds (3000 milliseconds)
-const autoSlideInterval = setInterval(autoSlide, 3000);
+let autoSlideInterval = setInterval(autoSlide, 3000);
 
 // Pause auto sliding when hovering over the carousel
-carousel.addEventListener('mouseover', () => {
+sliderContainer.addEventListener('mouseover', () => {
   clearInterval(autoSlideInterval);
 });
 
-// Resume auto sliding when mouse leaves the carousel
-carousel.addEventListener('mouseout', () => {
+// Resume auto sliding when the mouse leaves the carousel
+sliderContainer.addEventListener('mouseout', () => {
   autoSlideInterval = setInterval(autoSlide, 3000);
 });
+
+// Initial slide setup
+showSlide(currentIndex);
+slides[currentIndex].classList.add('active'); // Add 'active' class to the initial slide
