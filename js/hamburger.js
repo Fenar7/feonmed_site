2cac009e07e671b184882d7aded60a062efc7dc2
Fenar Khan <fenarkhan7@gmail.com>
2023-10-04 02:11:36 +0530

    finalised

--- js/hamburger.js
+++ js/hamburger.js
@@ -2,6 +2,9 @@ const openNav = document.querySelector('.open-nav')
 const closeBtn = document.querySelector('.close')
 const hamBg = document.querySelector('.hamburger')
 
+const search = document.querySelector('.search-icon');
+const searchBox = document.querySelector('.search-box-container');
+
 hamBg.addEventListener('click', () =>{
     openNav.classList.add('display-allow')
 })
@@ -9,4 +12,17 @@ hamBg.addEventListener('click', () =>{
 closeBtn.addEventListener('click', () =>{
     openNav.classList.remove('display-allow')
 })
+
+
+
+search.addEventListener('click', () =>{
+    if(searchBox.classList.contains('active')){
+        searchBox.classList.remove('active')
+    }
+    else{
+        searchBox.classList.add('active')
+    }
+})
+
+
 
